ddd1c710f9f6ccdffa0ca23e5cd34e12b6b244c2
Bruce Farthing <Bruce.Farthing@performanta.com>
2023-08-24 16:29:23 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AmethystCMS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
